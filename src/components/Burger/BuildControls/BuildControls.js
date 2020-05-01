36f17c8330b7a486b23e110cede09401f73da1f9
Duncan Moyo <duncanfmoyo@gmail.com>
2020-05-01 12:07:53 +0200

    connecting state to build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,8 +12,12 @@ const controls = [
 const BuildControls = (props) => {
   return (
     <div className={classes.BuildControls}>
-      {controls.map(ctrl => (
-        <BuildControl key={ctrl.label} label={ctrl.label} />
+      {controls.map((ctrl) => (
+        <BuildControl
+          key={ctrl.label}
+          label={ctrl.label}
+          added={() => props.ingredientAdded(ctrl.type)}
+        />
       ))}
     </div>
   );

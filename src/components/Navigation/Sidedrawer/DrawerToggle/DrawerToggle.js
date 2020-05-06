6e22c42dc6893e6da1f6c37455322bd03d2f5e24
Duncan Moyo <duncanfmoyo@gmail.com>
2020-05-06 11:10:29 +0200

    adding a humburger icon

--- src/components/Navigation/Sidedrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/Sidedrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import classses from './DrawerToggle.module.css'
 
 const DrawerToggle = (props) => (
-  <div onClick={props.clicked}>MENU</div>
+  <div className={classses.DrawerToggle} onClick={props.clicked}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
   
 )
 

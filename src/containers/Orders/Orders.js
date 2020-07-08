ea20549bed16078c228bdf70d87b63cb87c8f7a7
Duncan Moyo <duncanfmoyo@gmail.com>
2020-07-08 16:35:20 +0200

    outputting the orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Order from "../../components/Order/Order";
-import axios from '../../axios-orders';
-import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 class Orders extends Component {
   state = {
@@ -11,16 +11,16 @@ class Orders extends Component {
 
   componentDidMount() {
     axios
-      .get("./orders")
+      .get("./orders.json")
       .then((res) => {
-        const fetchedOrders = []
+        const fetchedOrders = [];
         for (let key in res.data) {
           fetchedOrders.push({
             ...res.data[key],
-            id: key
-          })
+            id: key,
+          });
         }
-        this.setState({ loading: false });
+        this.setState({ loading: false, orders: fetchedOrders });
       })
       .catch((err) => {
         this.setState({ loading: false });
@@ -30,8 +30,13 @@ class Orders extends Component {
   render() {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map((order) => (
+          <Order
+            key={order.id}
+            ingredients={order.ingredients}
+            price={+order.price}
+          />
+        ))}
       </div>
     );
   }

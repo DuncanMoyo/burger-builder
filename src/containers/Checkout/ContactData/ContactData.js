d9b65700032c7257116ddc29d234ee7c9cbcecc0
Duncan Moyo <duncanfmoyo@gmail.com>
2020-08-05 01:59:13 +0200

    working on order actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -92,7 +92,6 @@ class ContactData extends Component {
         valid: true,
       },
     },
-    loading: false,
     formIsValid: false,
   };
 
@@ -186,7 +185,7 @@ class ContactData extends Component {
         </Button>
       </form>
     );
-    if (this.state.loading) {
+    if (this.props.loading) {
       form = <Spinner />;
     }
     return (
@@ -201,14 +200,15 @@ class ContactData extends Component {
 const mapsStateToProps = state => {
   return {
     ings: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    loading: state.loading
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
   }
 }
 
-export default connect(mapsStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(mapsStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
